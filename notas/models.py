3a4af50b7564df38e91f97cba3011714e286638b
pildoras <contacto@pildorasdeprogramacion.com>
2026-08-10 20:53:08 -0500

    feat: agrega modelo Nota y registra la app

--- notas/models.py
+++ notas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Nota(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    creada = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
