104be06ae5b3e6f65caa36df83ce5d96ad274d4e
pildoras <contacto@pildorasdeprogramacion.com>
2026-08-10 21:23:35 -0500

    feat: agrega inicio con saludo personalizado

--- notas/views.py
+++ notas/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def inicio(request):
+    return HttpResponse("<h1>Mis Notas</h1><p>Tu espacio para pensar.</p>")
