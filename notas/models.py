0554aba78442ec0665af4e66c93fea2033603a50
pildoras <contacto@pildorasdeprogramacion.com>
2026-08-10 21:05:39 -0500

    feat: amplia el titulo a 200 caracteres segun revision

--- notas/models.py
+++ notas/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Nota(models.Model):
-    titulo = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=200)
     contenido = models.TextField()
     creada = models.DateTimeField(auto_now_add=True)
 
