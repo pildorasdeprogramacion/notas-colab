7f55cbe5fde8c0bcc2be07e3b5b2795a5ed6e459
pildoras <contacto@pildorasdeprogramacion.com>
2026-08-17 20:47:18 -0500

    feat: muestra el conteo de notas en el inicio

--- notas/views.py
+++ notas/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
-
+from notas.models import Nota
 
 def inicio(request):
-    return HttpResponse("<h1>Notas Colab</h1><p>Tus ideas, en orden</p><footer>Notas Colab 226</footer>")
+    notas = Nota.objects.count()
+    return HttpResponse(
+        "<h1>Notas Colab</h1><p>Tus ideas, en orden.</p>"
+        f"<p>Notas guardadas: {notas}</p>"
+        "<footer>Notas Colab 226</footer>"
+    )
